37feb8acbbd68822d06612fc11bc9f44815983f7
Chinguun19 <centillionc0919@gmail.com>
2024-12-11 20:39:49 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function WeatherApp() {
       setError("Please enter a city name.");
       return;
     }
-    setError(""); // Clear previous errors
+    setError(""); 
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
@@ -24,13 +24,12 @@ export default function WeatherApp() {
         throw new Error("City not found");
       }
       const data = await response.json();
-      console.log("Weather Data:", data); // Debugging: Check API response
+      console.log("Weather Data:", data); 
       setWeather(data);
     } catch (err) {
       setError(err.message);
       setWeather(null);
     }
-    const weatherData = data?.main || {}; 
   };
   
 
@@ -71,9 +70,9 @@ export default function WeatherApp() {
     <input onChange={(e) => setCity(e.target.value)}  value={city} placeholder="Enter a city" className="absolute z-10  h-input w-input left-inputLeft top-7 rounded-lg value={city}"></input>
     <button onClick={fetchWeather}  className="absolute top-1">Get Weather</button>
     <div className="  bg-white bg-opacity-75 h-city w-city justify-self-center align-self justify-center items-center flex rounded-xl relative" >
-     <div className="  text-black bg-custom-gradient bg-clip-text text-transparent absolute font-bold text-city top-cityTop left-cityLeft ">Kraków</div> 
-     <h1 className=" text-9xl absolute font-bold  bottom-temp left-10 bg-gradient-to-r from-slate-400 via-slate-800 to-slate-900 bg-clip-text text-transparent">{weather.main.temp}</h1>
-     <div className="text-mood font-bold text-moods bottom-moodBottom left-moodLeft absolute">Bright</div>
+     <div className="  text-black bg-custom-gradient bg-clip-text text-transparent absolute font-bold text-city top-cityTop left-cityLeft ">{weather.name}</div> 
+     <h1 className=" text-9xl absolute font-bold  bottom-temp left-10 bg-gradient-to-r from-slate-400 via-slate-800 to-slate-900 bg-clip-text text-transparent"> {Math.round(weather.main.temp)}°C</h1>
+     <div className="text-mood font-bold text-moods bottom-moodBottom left-moodLeft absolute">{weather.weather[0].main}</div>
      <img className=" absolute w-64 h-64 bottom-image-bottom" src="sun.png"></img>
       </div>
   </div>
