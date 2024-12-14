4a5b66dbcda3488d3a16d1f526bb504d268b47be
Chinguun19 <centillionc0919@gmail.com>
2024-12-14 19:26:11 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,40 @@ export default function WeatherApp() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState("");
+  const [results, setResults] = useState([])
 
-  const API_KEY = "408d4e797f164849c68729e28200d6ad";
+  const API_KEY = "9e8f5ac630a641fa986e0bd668216f13";
+
+
+  const fetchCityNames = (value) => {
+    fetch(`https://countriesnow.space/api/v0.1/countries`)
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.data.filter((country) => {
+          return (
+            value &&
+            country &&
+            country.cities &&
+            country.cities.some((city) => city.toLowerCase().includes(value.toLowerCase()))
+          );
+        });
+        setResults(results);
+      });
+  };
+  
+
+  fetchCityNames
+  console.log(results)
+
+
+
+
+
+
+
+
+
+
 
   const fetchWeather = async () => {
     if (!city) {
@@ -16,12 +48,14 @@ export default function WeatherApp() {
     }
     setError(""); 
     try {
+      
       const response = await fetch(
       
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+        
       );
       if (!response.ok) {
-        throw new Error("City not found");
+        throw new Error("City not found"); 
       }
       const data = await response.json();
       setWeather(data);
@@ -81,19 +115,34 @@ export default function WeatherApp() {
   };
 
   return (
-    <div className="w-full h-screen z-0 bg-black  overflow-x-hidden justify-center overflow-y-hidden flex">
+    <div className="w-full h-screen z-0 bg-gray-800  overflow-x-hidden justify-center overflow-y-hidden flex">
       <div className="justify-center items-center flex">
         <div className=" bg-weather w-[800px] h-[1200px] top-40 left-36 z-10 flex justify-center items-center rounded-tr-[50px] rounded-tl-[50px] ">
           <div className="">
             <img src="Vector.svg" className="absolute top-12 translate-x-3 z-50 ccolor- "></img>
           <input 
-            onChange={(e) => setCity(e.target.value)}
+             onChange={(e) => {
+              setCity(e.target.value);
+              fetchCityNames(e.target.value);
+            }}
             value={city}
             onKeyDown={handleKeyDown}
             placeholder="Search"
+            
             className="absolute z-10 h-[80px] w-[567px] left-input Left top-7 rounded-3xl box-shadow: 0px 12px 24px 0px #0000000F font-xl text-black text-[32px] focus:outline-none px-[50px] font-manrope font-[700] " 
           />
+         {results.slice(1, 4).map((country, index) => (
+  <div key={index}>
+    {country.cities.map((city, i) => (
+      <div key={i}>{city}</div>
+    ))}
+  </div>
+))}
+
+
+
           </div>
+
           {/* <button
             onClick={fetchWeather}
             className="absolute top-1 bg-blue-500 text-white px-4 py-2 rounded-lg"
@@ -105,17 +154,17 @@ export default function WeatherApp() {
             <div className="bg-white bg-opacity-75 h-city w-city flex justify-center items-center rounded-[48px] relative backdrop-blur-3xl">
               <div> </div>
               <div className="text-black font-manrope font-extrabold  bg-custom-gradient bg-clip-text text-transparent absolute text-[48px] top-cityTop left-cityLeft">
-                {weather.name}
+                {weather.city.name}
               </div>
               <h1 className="font-manrope font-extrabold text-[144px] absolute  bottom-temp left-10 bg-gradient-to-r from-slate-400 via-slate-800 to-slate-900 bg-clip-text text-transparent text-black">
-                {Math.round(weather.main.temp)}°
+              {Math.round(weather.list[0].main.temp)}°
               </h1>
               <div className="text-mood font-extrabold text-[24px] font-manrope bottom-moodBottom left-moodLeft absolute">
-                {weather.weather[0].main}
+              {weather.list[1].weather[0].main}
               </div>
               <img
                 className="absolute w-64 h-64 bottom-image-bottom"
-                src={getWeatherImage(weather.weather[0].main)}></img>
+                src={getWeatherImage(weather.list[0].weather[0].main)}></img>
                 <img className="absolute w-[261px] h-[262px] opacity-[50%] bottom-image-bottom" src="shadow.png">
               
                 </img>
@@ -133,19 +182,19 @@ export default function WeatherApp() {
           {error && <div className="text-red-500 absolute top-16">{error}</div>}
           {weather && (
             <div className="bg-night bg-opacity-75 h-city w-city flex justify-center items-center relative rounded-[48px]">
-              <div>setCurrentDate(getDate()) </div>
+              <div></div>
               <div className="text-white font-manrope font-extrabold  bg-custom-gradient bg-clip-text text-transparent absolute text-city top-cityTop left-cityLeft">
-                {weather.name}
+                {weather.city.name}
               </div>
               <h1 className="font-manrope font-extrabold  text-[144px] absolute bottom-temp left-10 bg-gradient-to-r from-slate-400 via-slate-800 to-slate-900  bg-clip-text text-transparent text-white">
-                {Math.round(weather.main.temp_min)}°
+                {Math.round(weather.list[4].main.temp)}°
               </h1>
               <div className="text-purple font-bold text-moods bottom-moodBottom left-moodLeft absolute">
-                {weather.weather[0].main}
+                {weather.list[4].weather[0].main}
               </div>
               <img
                 className="absolute w-64 h-64 bottom-image-bottom"
-                src={getNightImage(weather.weather[0].main)}></img>
+                src={getNightImage(weather.list[4].weather[0].main)}></img>
                 <img className="absolute w-[277px] h-[277px] bottom-image-bottom" src="nightShadow.png"></img>
             </div>
           )}
